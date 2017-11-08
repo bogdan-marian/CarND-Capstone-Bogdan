9befa17c1e0ed871a13019df52dc2b3afebf1dd8
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2017-11-08 15:33:32 -0800

    implemented Waypoint Updater Node (Partial)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,8 +3,10 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32
 
 import math
+import tf
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -22,7 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+POSITION_SEARCH_RANGE = 10  # Number of waypoints to search current position back and forth
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -35,18 +37,29 @@ class WaypointUpdater(object):
 
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        self.next_wp_pub = rospy.Publisher('/next_wp', Int32, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.waypoints = None
 
         rospy.spin()
 
-    def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+    
 
-    def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+    def pose_cb(self, msg):
+        self.current_pose = msg
+        if self.waypoints:
+            next_waypoint_index = self.update_next_waypoint()
+            self.next_wp_pub.publish(Int32(next_waypoint_index))
+
+    def waypoints_cb(self, lane):
+	rospy.loginfo(rospy.get_caller_id() + "Hello waypoints: ")
+        if hasattr(self, 'waypoints') and self.waypoints != lane.waypoints:
+            self.waypoints = lane.waypoints
+            # full search current position on each update of waypoints
+            self.next_waypoint_index = None 
+            
+	
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -69,6 +82,58 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+    def current_yaw(self):
+        quaternion = (
+            self.current_pose.pose.orientation.x,
+            self.current_pose.pose.orientation.y,
+            self.current_pose.pose.orientation.z,
+            self.current_pose.pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        return euler[2]
+
+    def euclidean_distance_2d(self, position1, position2):
+        a = position1
+        b = position2
+        return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+
+    def get_closest_waypoint(self):
+        min_dist = 100000
+        min_ind = 0
+        ind = 0
+
+        start = 0
+        end = len(self.waypoints)
+        if (self.next_waypoint_index):
+            start = max (self.next_waypoint_index - POSITION_SEARCH_RANGE, 0)
+            end = min (self.next_waypoint_index + POSITION_SEARCH_RANGE, end)
+
+        position1 = self.current_pose.pose.position
+        for i in range(start, end):
+            position2 = self.waypoints[i].pose.pose.position
+            dist = self.euclidean_distance_2d(position1, position2)
+            if dist < min_dist:
+                min_dist = dist
+                min_ind = i
+        return min_ind
+
+
+    def update_next_waypoint(self):
+        ind = self.get_closest_waypoint()
+
+        map_x = self.waypoints[ind].pose.pose.position.x
+        map_y = self.waypoints[ind].pose.pose.position.y
+
+        x = self.current_pose.pose.position.x
+        y = self.current_pose.pose.position.y
+
+        yaw = self.current_yaw()
+
+        x_car_system = ((map_x-x) * math.cos(yaw) + (map_y-y) * math.sin(yaw))
+        if ( x_car_system < 0. ):
+            ind += 1
+        self.next_waypoint_index = ind
+        return ind
 
 
 if __name__ == '__main__':
